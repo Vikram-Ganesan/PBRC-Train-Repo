37234e56afa83df59ad7e4b135ffc76c583b362e
Vikram G <164937259+Vikram-Ganesan@users.noreply.github.com>
2024-05-07 22:03:12 +0530

    Update script.js
    Few bugs fixed

--- script.js
+++ script.js
@@ -3,8 +3,7 @@ barContainer.addEventListener("click", (e) => {
     e.preventDefault();
     let menuList = document.querySelectorAll("#menuList");
     barContainer.classList.toggle("changeBar");
-    console.log(menuList);
-    menuList.forEach((ele, i) => {
+    menuList.forEach((ele) => {
         console.log(ele);
         ele.classList.toggle("menuList");
     })
@@ -12,46 +11,41 @@ barContainer.addEventListener("click", (e) => {
 // !SHOW LIST
 
 let list = document.getElementById("stationList");
-let s_list = document.getElementById("stationList");
-
-console.log(list);
+let s_list = document.getElementById("stationList1");
 let input = document.getElementById("source-station");
 input.addEventListener("focus", (f) => {
     console.log(f.target.getAttribute("id"));
     if (f.target.getAttribute("id") == "source-station") {
         list.style.display = "block";
 
-    }
-
-})
-s_list.addEventListener('click', function (e) {
+        
+list.addEventListener('click', function (e) {
     if (e.target && e.target.nodeName === "SPAN") {
         console.log(e.target.getAttribute('value'));
         input.value = e.target.getAttribute('value');
         list.style.display = 'none'; // Hide the list
         
     }
-    // list.removeEventListener("click", () => {});
 
 });
+    }
+
+})
 let destination = document.getElementById("destination-station");
 destination.addEventListener("focus", (e) => {
     console.log(e.target.getAttribute('id'));
     if (e.target.getAttribute('id') === "destination-station") {
-        list.setAttribute("id", "stationList1");
-        list.style.display = "block";
-        list.addEventListener('click', function (f) {
+        // list.setAttribute("id", "stationList1");
+        s_list.style.display = "block";
+        s_list.addEventListener('click', function (f) {
             if (f.target && f.target.nodeName === "SPAN") {
                 destination.value = f.target.getAttribute('value');
-                list.style.display = 'none';
+                s_list.style.display = 'none';
               
             }
-            list.setAttribute("id", "stationList");
 
         });
-       
-        
-        list.removeEventListener("click", () => {});
+
     }
 });
 
